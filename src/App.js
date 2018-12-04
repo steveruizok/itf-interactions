84df3be4b843d5a53b93d25f1ccb2306e9b91b89
Steve <steveruizok@gmail.com>
2018-12-04 10:57:13 +0000

    dropped suspense to only one component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import './styles.css'
 
 import Menu from './components/Menu'
@@ -8,6 +8,17 @@ import { PageIndicatorStory } from './components/pager/PageIndicator'
 import { PagerStory } from './components/pager/Pager'
 import { CalendarStory } from './components/Calendar'
 
+import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks'
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache(),
+})
+
 const App = () => {
 	return (
 		<div className="App">
@@ -16,7 +27,13 @@ const App = () => {
 			<PagerButtonStory name="Pager Button" />
 			<PageIndicatorStory name="Page Indicator" />
 			<PagerStory name="Pager" />
-			<CalendarStory name="Calendar" />
+			<ApolloProvider client={client}>
+				<ApolloHooksProvider client={client}>
+					<Suspense fallback={<div>Loading...</div>}>
+						<CalendarStory name="Calendar" />
+					</Suspense>
+				</ApolloHooksProvider>
+			</ApolloProvider>
 		</div>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-
-import { ApolloProvider } from 'react-apollo'
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks'
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
 
 import App from './App'
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
-
-const client = new ApolloClient({
-	link: httpLink,
-	cache: new InMemoryCache(),
-})
-
 const rootElement = document.getElementById('root')
-ReactDOM.render(
-	<ApolloProvider client={client}>
-		<ApolloHooksProvider client={client}>
-			<Suspense fallback={<div>Loading...</div>}>
-				<App />
-			</Suspense>
-		</ApolloHooksProvider>
-	</ApolloProvider>,
-	rootElement
-)
+ReactDOM.render(<App />, rootElement)
